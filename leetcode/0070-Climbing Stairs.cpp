8cf73318c72ba394e52dc5fae44194dc91be4b39
wangxinghust <31427118+wangxinghust@users.noreply.github.com>
2019-06-06 11:51:46 +0800

    刷到第70题

--- leetcode/0070-Climbing Stairs.cpp
+++ leetcode/0070-Climbing Stairs.cpp
@@ -1,6 +1,20 @@
 class Solution {
 public:
-	int climbStairs(int n) {
 
+	//斐波那契数列 递归或者dp
+	//Runtime: 4 ms, faster than 85.73% of C++ online submissions for Climbing Stairs.
+	//Memory Usage : 8.3 MB, less than 65.82 % of C++ online submissions for Climbing Stairs.
+	int climbStairs(int n) {
+		if (n < 3) {
+			return n;
+		}
+		int first = 1;
+		int second = 2;
+		for (int i = 3; i <= n; i++) {
+			int tmp = second;
+			second = first + second;
+			first = tmp;
+		}
+		return second;
 	}
 };
