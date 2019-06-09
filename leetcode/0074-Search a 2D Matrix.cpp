eeb6b1f4c711cecea144100c3728949ab2ace967
wangxinghust <31427118+wangxinghust@users.noreply.github.com>
2019-06-09 17:58:15 +0800

    增加74解法，添加75题

--- leetcode/0074-Search a 2D Matrix.cpp
+++ leetcode/0074-Search a 2D Matrix.cpp
@@ -46,12 +46,34 @@ public:
 		}
 		return false;
 	}
+
+	//当做一个排序的一维数组来处理
+	//Runtime: 8 ms, faster than 97.98% of C++ online submissions for Search a 2D Matrix.
+	//Memory Usage : 9.9 MB, less than 54.99 % of C++ online submissions for Search a 2D Matrix.
+	bool searchMatrix2(vector<vector<int>>& matrix, int target) {
+		if (matrix.empty() || matrix[0].empty()) return false;
+		int col = matrix[0].size();
+		int left = 0, right = matrix.size() * col - 1;
+		while (left <= right) {
+			int mid = left + ((right - left) >> 1);
+			if (matrix[mid / col][mid % col] < target) {
+				left = mid + 1;
+			}
+			else if (matrix[mid / col][mid % col] > target) {
+				right = mid - 1;
+			}
+			else {
+				return true;
+			}
+		}
+		return false;
+	}
 };
 
-int main(int argc, char* argv[]) {
-	vector<vector<int>> matrix = { {1} };
-	Solution s;
-	bool b = s.searchMatrix(matrix, 0);
-	cout << b << endl;
-	return 0;
-}
+//int main(int argc, char* argv[]) {
+//	vector<vector<int>> matrix = { {1} };
+//	Solution s;
+//	bool b = s.searchMatrix(matrix, 0);
+//	cout << b << endl;
+//	return 0;
+//}
