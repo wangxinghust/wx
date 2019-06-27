cab34de96323f60edef26cf2b2d1f27ebcc5084e
wangxing <wangxinghust@qq.com>
2019-06-27 11:38:20 +0800

    update to problem 138

--- leetcode_from_problem107/0136-Single Number.cpp
+++ leetcode_from_problem107/0136-Single Number.cpp
@@ -1,11 +1,14 @@
 #include <vector>
 #include <set>
 #include <list>
+#include <unordered_set>
 using namespace std;
 
 // 按题目要求只能用位运算来解
 
 // leetcode Approach 1: List operation
+// Runtime: 380 ms, faster than 5.15% of C++ online submissions for Single Number.
+// Memory Usage : 11.2 MB, less than 21.78 % of C++ online submissions for Single Number.
 class Solution {
 public:
 	int singleNumber(vector<int>& nums) {
@@ -20,6 +23,58 @@ public:
 };
 
 // leetcode Approach 2: Hash Table
+// hash_set 非标准 换unordered_set
+// Runtime: 20 ms, faster than 41.10% of C++ online submissions for Single Number.
+// Memory Usage : 11.4 MB, less than 15.99 % of C++ online submissions for Single Number.
 class Solution2 {
+	int singleNumber(vector<int>& nums) {
+		unordered_set<int> hash_table;
+		for (int i : nums) {
+			if (hash_table.find(i) != hash_table.end()) hash_table.erase(i);
+			else hash_table.insert(i);
+		}
+		return *(hash_table.begin());
+	}
+};
 
+// leetcode Approach 3: Math
+// 2*(a+b+c)-(a+a+b+b+c)=c
+// Runtime: 12 ms, faster than 96.86 % of C++ online submissions for Single Number.
+// Memory Usage : 11.7 MB, less than 5.68 % of C++ online submissions for Single Number.
+class Solution3 {
+	int singleNumber(vector<int>& nums) {
+		unordered_set<int> set;
+		int sum = 0;
+		for (int i : nums) {
+			sum += i;
+			set.insert(i);
+		}
+		int set_sum = 0;
+		for (int i : set) {
+			set_sum += 2 * i;
+		}
+		return set_sum - sum;
+	}
+};
+
+// leetcode Approach 4: Bit Manipulation
+// Runtime: 16 ms, faster than 82.19% of C++ online submissions for Single Number.
+// Memory Usage : 9.8 MB, less than 30.12 % of C++ online submissions for Single Number.
+class Solution4 {
+	int singleNumber(vector<int>& nums) {
+		int a = 0;
+		for (int i : nums) {
+			a ^= i;
+		}
+		return a;
+	}
+
+	// Runtime: 12 ms, faster than 96.86% of C++ online submissions for Single Number.
+	// Memory Usage : 9.6 MB, less than 87.92 % of C++ online submissions for Single Number.
+	int singleNumber2(vector<int>& nums) {
+		for (int i = 1; i < nums.size(); i++) {
+			nums[0] ^= nums[i];
+		}
+		return nums[0];
+	}
 };
