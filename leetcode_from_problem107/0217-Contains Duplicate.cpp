de0af8b63af8d89dc068c4886cb6f9f52e4c12db
wangxinghust <514122146@qq.com>
2019-07-10 22:40:33 +0800

    update to problem 217

---
+++ leetcode_from_problem107/0217-Contains Duplicate.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+#include <algorithm>
+#include <set>
+using namespace std;
+
+// Approach #1 (Naive Linear Search) [Time Limit Exceeded]
+
+// Approach #2 (Sorting)
+class Solution {
+public:
+	bool containsDuplicate(vector<int>& nums) {
+		if (nums.size() < 2) return false;
+		sort(nums.begin(), nums.end());
+		for (int i = 0; i < nums.size() - 1; ++i) {
+			if (nums[i] == nums[i + 1]) return true;
+		}
+		return false;
+	}
+};
+
+// Approach #3 (Hash Table)
+// Runtime: 56 ms, faster than 22.47% of C++ online submissions for Contains Duplicate.
+// Memory Usage : 16.3 MB, less than 68.74 % of C++ online submissions for Contains Duplicate.
+class Solution {
+public:
+	bool containsDuplicate(vector<int>& nums) {
+		if (nums.size() < 2) return false;
+		set<int> newNums;
+		for (int x : nums) {
+			if (newNums.find(x) != newNums.end()) return true;
+			newNums.insert(x);
+		}
+		return false;
+	}
+};
---
+++ leetcode_from_problem107/0218-The Skyline Problem.cpp
@@ -0,0 +1,8 @@
+#include <vector>
+using namespace std;
+class Solution {
+public:
+	vector<vector<int>> getSkyline(vector<vector<int>>& buildings) {
+
+	}
+};
