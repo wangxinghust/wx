c1e376a400e33851014bfe9e552d5dc655743ddb
X W <514122146@qq.com>
2021-08-29 23:07:16 +0800

    feat(java): add problem 1235 approach 2

--- src/main/java/org/wx/problems/Solution1235.java
+++ src/main/java/org/wx/problems/Solution1235.java
@@ -17,6 +17,7 @@ public class Solution1235 {
 
     /**
      * 排序还是利用java的排序，新增可排序对象
+     * 优化想法是类似mysql普通索引，对startTime字段排序，根据主键id也就是数组下标查找endTime和profit值，这样就可以变成两个数组的排序，手写快排实现
      */
     private class Pair implements Comparable<Pair> {
         int st, et, profit;
@@ -27,6 +28,7 @@ public class Solution1235 {
             this.profit = profit;
         }
 
+        @Override
         public int compareTo(Pair o) {
             if (this.st != o.st) {
                 return this.st - o.st;
@@ -36,6 +38,13 @@ public class Solution1235 {
         }
     }
 
+    /**
+     * Runtime: 21 ms
+     * Memory Usage: 71 MB
+     * 将Pair改为static对象后，内用占用减少
+     * Runtime: 13 ms, faster than 91.88% of Java online submissions for Maximum Profit in Job Scheduling.
+     * Memory Usage: 47.2 MB, less than 90.45% of Java online submissions for Maximum Profit in Job Scheduling.
+     */
     public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
         Pair[] inp = new Pair[startTime.length];
 
@@ -85,6 +94,68 @@ public class Solution1235 {
         while (l < r) {
             int m = (l + r) >>> 1;
             if (a[m].st < x) l = m + 1;
+            else r = m;
+        }
+        return l;
+    }
+
+    /**
+     * 方案2 手写快排实现startTime及下标排序
+     * 但手写快排效率远不如java优化后的归并排序，内存消耗是少了不少
+     * Runtime: 382 ms
+     * Memory Usage: 49 MB
+     */
+    public int jobScheduling2(int[] startTime, int[] endTime, int[] profit) {
+        int[] index = new int[startTime.length];
+        for (int i = 0; i < index.length; i++) {
+            index[i] = i;
+        }
+        quickSort(startTime, index, 0, startTime.length - 1);
+        int[] dp = new int[startTime.length + 1];
+        dp[startTime.length - 1] = profit[index[startTime.length - 1]];
+        for (int i = startTime.length - 2; i >= 0; i--) {
+            dp[i] = Math.max(dp[i + 1], profit[index[i]] + dp[upperBound(startTime, endTime[index[i]], i, startTime.length)]);
+        }
+        return dp[0];
+    }
+
+    private void quickSort(int[] startTime, int[] index, int low, int high) {
+        int p, i, j;
+        if (low >= high) {
+            return;
+        }
+        // p为基准数，这里取基准数为待排数组的第一个
+        p = startTime[low];
+
+        i = low;
+        j = high;
+        while (i < j) {
+            while (startTime[j] >= p && i < j) {
+                j--;
+            }
+            while (startTime[i] <= p && i < j) {
+                i++;
+            }
+            int temp = startTime[i];
+            startTime[i] = startTime[j];
+            startTime[j] = temp;
+            temp = index[i];
+            index[i] = index[j];
+            index[j] = temp;
+        }
+        startTime[low] = startTime[i];
+        startTime[i] = p;
+        int t = index[low];
+        index[low] = index[i];
+        index[i] = t;
+        quickSort(startTime, index, low, i - 1);
+        quickSort(startTime, index, i + 1, high);
+    }
+
+    private int upperBound(int[] startTime, int value, int l, int r) {
+        while (l < r) {
+            int m = (l + r) >>> 1;
+            if (startTime[m] < value) l = m + 1;
             else r = m;
         }
         return l;
