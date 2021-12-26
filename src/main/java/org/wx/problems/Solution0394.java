7e13ad5cfe7cc0328c44300100d52dadcfaf5c2a
wangxinghust <wangxinghust@qq.com>
2021-12-26 18:13:27 +0800

    feat(java): add problem 394

---
+++ src/main/java/org/wx/problems/Solution0394.java
@@ -0,0 +1,31 @@
+package org.wx.problems;
+
+import java.util.Stack;
+
+public class Solution0394 {
+    public String decodeString(String s) {
+        Stack<Integer> repeats = new Stack<Integer>();
+        char[] chs = s.toCharArray();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < chs.length; i++) {
+            if (Character.isDigit(chs[i])) {
+                int num = chs[i] - '0';
+                while (chs[++i] != '[') {
+                    num = num * 10 + chs[i] - '0';
+                }
+                repeats.push(num);
+                repeats.push(sb.length());
+            } else if (chs[i] == ']') {
+                int index = repeats.pop();
+                int num = repeats.pop();
+                String str = sb.toString().substring(index);
+                for (int j = num; j > 1; j--) {
+                    sb.append(str);
+                }
+            } else {
+                sb.append(chs[i]);
+            }
+        }
+        return sb.toString();
+    }
+}
