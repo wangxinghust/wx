26587cd5bd959106958dc40a8a784afb1377aa14
xing <wangxinghust@qq.com>
2022-06-04 23:50:59 +0800

    problem 929

---
+++ src/main/java/org/wx/interview/Solution0929.java
@@ -0,0 +1,21 @@
+package org.wx.interview;
+
+import java.util.Arrays;
+
+public class Solution0929 {
+    public int numUniqueEmails(String[] emails) {
+        long cnt = Arrays.stream(emails).map(str -> {
+            int indexOfAt = str.indexOf('@');
+            String localName = str.substring(0, indexOfAt);
+            String domainName = str.substring(indexOfAt + 1);
+            int indexOfPlus = localName.indexOf('+');
+            if (indexOfPlus >= 0) {
+                localName = localName.substring(0, indexOfPlus);
+            }
+            localName = localName.replaceAll("\\.", "");
+            System.out.println(localName + '@' + domainName);
+            return localName + '@' + domainName;
+        }).distinct().count();
+        return (int) cnt;
+    }
+}
