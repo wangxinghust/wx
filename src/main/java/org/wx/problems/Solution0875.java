11ac37dc37d4ceb59c7ab19224b7ac8ea97370a3
xing <wangxinghust@qq.com>
2022-06-07 23:37:59 +0800

    problem 875

---
+++ src/main/java/org/wx/problems/Solution0875.java
@@ -0,0 +1,35 @@
+package org.wx.problems;
+
+/**
+ * 考察二分查找，有上下限
+ */
+public class Solution0875 {
+    public int minEatingSpeed(int[] piles, int h) {
+        int low = 1;
+        int high = 0;
+        for (int pile : piles) {
+            high = Math.max(high, pile);
+        }
+        int k = high;
+        while (low < high) {
+            int speed = (high - low) / 2 + low;
+            long time = getTime(piles, speed);
+            if (time <= h) {
+                k = speed;
+                high = speed;
+            } else {
+                low = speed + 1;
+            }
+        }
+        return k;
+    }
+
+    public long getTime(int[] piles, int speed) {
+        long time = 0;
+        for (int pile : piles) {
+            int curTime = (pile + speed - 1) / speed;
+            time += curTime;
+        }
+        return time;
+    }
+}
