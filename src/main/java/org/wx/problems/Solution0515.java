eb84dc9a5bcd0671f47808db21f392d83917141a
xing <wangxinghust@qq.com>
2022-06-24 10:23:03 +0800

    problem 515

---
+++ src/main/java/org/wx/problems/Solution0515.java
@@ -0,0 +1,78 @@
+package org.wx.problems;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Solution0515 {
+
+    //Definition for a binary tree node.
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public List<Integer> largestValues(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        if (root == null) {
+            return res;
+        }
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int rowMax = Integer.MIN_VALUE;
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                rowMax = Math.max(rowMax, node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            res.add(rowMax);
+        }
+        return res;
+    }
+
+    // 官解1 dfs 比较有意思 res数组既是结果，下标又对应着层高，还使用了
+    public List<Integer> largestValues2(TreeNode root) {
+        if (root == null) {
+            return new ArrayList<Integer>();
+        }
+        List<Integer> res = new ArrayList<Integer>();
+        dfs(res, root, 0);
+        return res;
+    }
+
+    public void dfs(List<Integer> res, TreeNode root, int curHeight) {
+        if (curHeight == res.size()) {
+            res.add(root.val);
+        } else {
+            res.set(curHeight, Math.max(res.get(curHeight), root.val));
+        }
+        if (root.left != null) {
+            dfs(res, root.left, curHeight + 1);
+        }
+        if (root.right != null) {
+            dfs(res, root.right, curHeight + 1);
+        }
+    }
+}
