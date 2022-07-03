78875e4acb0f75fb76b3563d63d5ab9d0cf3341f
xing <wangxinghust@qq.com>
2022-07-03 17:19:08 +0800

    lc

--- src/main/java/org/wx/problems/Solution0015.java
+++ src/main/java/org/wx/problems/Solution0015.java
@@ -33,4 +33,39 @@ public class Solution0015 {
         }
         return ans;
     }
+
+    // 排序+双指针比二分查找会快不少，复杂度更优
+    public List<List<Integer>> threeSum2(int[] nums) {
+        int n = nums.length;
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        for (int first = 0; first < nums.length - 2; first++) {
+            // 注意此处的跳过重复枚举元素写法
+            if (first > 0 && nums[first] == nums[first - 1]) {
+                continue;
+            }
+            int third = nums.length - 1;
+            int target = -nums[first];
+            for (int second = first + 1; second < nums.length - 1; second++) {
+                if (second > first + 1 && nums[second] == nums[second - 1]) {
+                    continue;
+                }
+                while (second < third && nums[second] + nums[third] > target) {
+                    --third;
+                }
+                // 局部优化，考虑到结果都大于target，增大second还是同样的结果，可以break跳出
+                if (second == third) {
+                    break;
+                }
+                if (nums[second] + nums[third] == target) {
+                    List<Integer> row = new ArrayList<>();
+                    row.add(nums[first]);
+                    row.add(nums[second]);
+                    row.add(nums[third]);
+                    res.add(row);
+                }
+            }
+        }
+        return res;
+    }
 }
