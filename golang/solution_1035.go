a7236d449c084c9e49cc2fb9e8f4675fca133f5d
wangxinghust <wangxinghust@qq.com>
2024-08-11 19:33:12 +0800

    Feature: solution 1035

---
+++ golang/solution_1035.go
@@ -0,0 +1,19 @@
+package main
+
+func maxUncrossedLines(nums1 []int, nums2 []int) int {
+	m, n := len(nums1), len(nums2)
+	dp := make([][]int, m+1)
+	for i := range dp {
+		dp[i] = make([]int, n+1)
+	}
+	for i, v := range nums1 {
+		for j, w := range nums2 {
+			if v == w {
+				dp[i+1][j+1] = dp[i][j] + 1
+			} else {
+				dp[i+1][j+1] = max(dp[i][j+1], dp[i+1][j])
+			}
+		}
+	}
+	return dp[m][n]
+}
